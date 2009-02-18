73ca7441a89ad9ff26943556d190bc26f3b030ae
wosmvp <wosmvp@gmail.com>
2009-02-18 08:45:08 +0800

    improve performance > 13%

--- lib/ipparse.rb
+++ lib/ipparse.rb
@@ -22,13 +22,16 @@ class IPParse
 
   protected
   def self.dichotomizing(arg,ip)
-   cen = (arg.length/2).to_i
-   x = arg[cen].split(/\s+/,3)
+    size = arg.size
+    cen  = size/2
 
-   return (x[0]..x[1]).include?(ip) ? x[2] : false if arg.size == 1
-   return dichotomizing(arg[0...cen],ip)           if x[0] > ip
-   return dichotomizing(arg[cen...arg.length],ip)  if x[1] < ip
-   return x[2]
+    # '001.053.000.000 001.153.255.255 IANA' < '001.053.000.000' + '1'
+    # reduce 'string'.split call
+    return dichotomizing(arg[0...cen],ip)           if size != 1 && (arg[cen] > ip + '1')
+    x = arg[cen].split(/\s+/,3)
+    return (x[0]..x[1]).include?(ip) ? x[2] : false if size == 1
+    return dichotomizing(arg[cen...size],ip)        if x[1] < ip
+    return x[2]
   end
 
   def self.format(ip)
--- test/test_ipparse.rb
+++ test/test_ipparse.rb
@@ -4,8 +4,18 @@ require 'test/unit'
 class TestIPParse < Test::Unit::TestCase
   def test_parse
     assert_equal IPParse.parse('222.153.3'),false
-    assert_equal IPParse.parse('222.15.153.3'),"江西省云南市"
-    assert_equal IPParse.parse('24.38.192.2'),"美国"
+    ips = {
+      "178.61.84.32 " => "Unknown",
+      "218.58.88.175" => "山东省青岛市开发区",
+      "212.1.1.1    " => "意大利",
+      "192.245.148.0" => "阿尔及利亚",
+      "116.254.254.0" => "广东省广州市",
+      "116.254.254.0" => "广东省广州市",
+      "16.34.110.150" => "美国",
+      "222.15.153.3"  => "江西省云南市",
+      "24.38.192.2"   => "美国"
+    }
+    ips.map {|k,v| assert_equal IPParse.parse(k),v}
   end
 
   def test_format
