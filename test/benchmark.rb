1648dd3e5090f7d14b2fd99fb101c8de71910465
quake <quake@quake-laptop.(none)>
2009-02-19 18:17:39 +0800

    add benchmark script

---
+++ test/benchmark.rb
@@ -0,0 +1,10 @@
+require File.join(File.dirname(__FILE__),"..","lib","ipparse.rb")
+require 'benchmark'
+
+ips = (1..100000).map { Array.new(4){|i| rand(255)}.join(".") }
+
+Benchmark.bmbm do |x|
+  x.report("IPParse") {ips.each{|ip| IPParse.parse(ip) }}
+end
+
+
