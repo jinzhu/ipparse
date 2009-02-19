82b99af36713434505ee093291a0914c7136185a
wosmvp <wosmvp@gmail.com>
2009-02-19 18:52:16 +0800

    update convert_ip_format script

--- lib/convert_ip_format.rb
+++ lib/convert_ip_format.rb
@@ -1,28 +1,26 @@
 # IP Data from PHPWind
 system("find #{File.dirname(__FILE__)}/../data -iname '*txt' -exec dos2unix \{\} \\;")
-system("find #{File.dirname(__FILE__)}/../data -iname '*txt' -exec sed -i 's/*/255/g' \{\} \\;")
-system("find #{File.dirname(__FILE__)}/../data -iname '*txt' -exec sed -i '/^$/d' \{\} \\;")
 
 def format(ip)
-  ip.to_s.split('.').inject([]) do |s,x|
-    s << x.rjust(3,'0')
-  end.join('.')
+  ip.to_s.gsub(/\*/,'255').scan(/\d+/).map{|x| x.rjust(3, '0')}.join('.')
 end
 
 file = Dir.new("#{File.dirname(__FILE__)}/../data").find.to_a.select {|x| x =~ /\w/}
 
 file.map do |z|
-  h = File.new("#{File.dirname(__FILE__)}/../data/#{z}").to_a
+  currentfile = File.open("#{File.dirname(__FILE__)}/../data/#{z}") {|m| m.to_a}
   prefix = (z =~ /^0\.txt$/ ? '' : z.split('.')[0].rjust(3,'0') + '.')
+  tmpfile = []
 
-  h.each_with_index do |x,i|
+  currentfile.each_with_index do |x,i|
+    next if x.strip.empty?
     x = x.split(/\s+/,3)
     x[0] = prefix + format(x[0])
     x[1] = prefix + format(x[1])
-    h[i] = x.join("\s")
+    tmpfile << x.join("\s")
   end
 
   File.open("#{File.dirname(__FILE__)}/../data/#{z}",'w+') do |x|
-    x << h
+    x << tmpfile
   end
 end
