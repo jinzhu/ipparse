fb44f51b836f0cbe9aa110dc74a33437d916dcf1
ZhangJinzhu <wosmvp@gmail.com>
2009-09-16 23:30:11 +0800

    Ruby1.9 compatible

--- lib/ipparse.rb
+++ lib/ipparse.rb
@@ -1,4 +1,5 @@
-$KCODE = 'UTF8'
+# -*- coding: utf-8 -*-
+$KCODE = 'UTF8' if RUBY_VERSION < '1.9'
 # IPV4
 class IPParse
   @@data    ||= []
@@ -7,7 +8,7 @@ class IPParse
     return false unless ip.to_s =~ /(\d+\.){3}\d+/
     ip = ip.scan(/\d+/).map{|x| x.rjust(3, '0')}.join('.')
 
-    return dichotomizing(@@data[ip[0,3].to_i] ||= file_to_a("#{File.dirname(__FILE__)}/../data/#{ip[0,3].to_i}.txt"),ip) || dichotomizing(@@data[0] ||= file_to_a("#{File.dirname(__FILE__)}/../data/0.txt"),ip) || "Unknown"
+    return dichotomizing(@@data[ip[0,3].to_i] ||= file_to_a("#{File.dirname(__FILE__)}/../data/#{ip[0,3].to_i}.txt"),ip) ||dichotomizing(@@data[0] ||= file_to_a("#{File.dirname(__FILE__)}/../data/0.txt"),ip) || "Unknown"
   end
 
   protected
@@ -16,7 +17,7 @@ class IPParse
     cur  = arg[cen]
 
     # arg.size = 0 || 1
-    return (cur && (cur[0,15]..cur[16,15]).include?(ip)) ? cur[32...-1] : false  if cen == 0
+    return cur && cur[0,15] <= ip && cur[16,15] >= ip ? cur[32...-1] : false  if cen == 0
     return dichotomizing(arg[0...cen],ip)        if cur[0,15]  > ip
     return dichotomizing(arg[cen+1..-1],ip)      if cur[16,15] < ip
     return arg[cen][32...-1]
--- test/test_ipparse.rb
+++ test/test_ipparse.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 require File.join(File.dirname(__FILE__),"..","lib","ipparse.rb")
 require 'test/unit'
 
